52c03d28a5d71f0d7ed3089cd5663fe1bcf8d960
arsivs <50826181+arsivs@users.noreply.github.com>
2024-10-18 22:55:35 +0300

    Add files via upload

--- script/key_letter.js
+++ script/key_letter.js
@@ -1,14 +1,2 @@
 
 
-function _line(){
-	let next_content = _Document.querySelector('.letter .txt_wrt .next'),
-		line = _Document.querySelector('.letter .txt_wrt .txt'),
-		light = next_content.querySelector('.light .x2'),
-		l_top = (next_content.scrollTop > light.offsetHeight) ? light.offsetTop - next_content.scrollTop : light.offsetTop,
-		l_left = light.offsetLeft;
-	line.style.animation = "none";
-	line.style.top = l_top + 'px';
-	line.style.left = l_left + 'px';
-	line.offsetWidth; /* Animation Reset */
-	line.style.animation = "line 1s 2s infinite";
-}
