9f254b6c2ff59df9b891f76bebe84fa02fdb577b
arsivs <50826181+arsivs@users.noreply.github.com>
2024-10-24 22:24:40 +0300

    Add files via upload

--- script/load.js
+++ script/load.js
@@ -175,7 +175,7 @@ function bg_popup_close(){
 }
 
 function _focus(add, key){
-	if( ( 48 >= key <= 57  ) || ( 96 >= key <= 105  ) )
+	if( (key >= 48 && key <= 57) || (key >= 96 && key <= 105) )
 		return false;
 	let _key = _Document.querySelector('.keyboard div[data-key="'+key+'"]');
 	if(_key.getAnimations()[0]){
